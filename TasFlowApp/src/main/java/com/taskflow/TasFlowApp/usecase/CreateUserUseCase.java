2480cfc32bdc7e2ba6eb259333857e25cc647e83
Gabriel Verga <gabrielverga@MacBook-Air-de-Gabriel.local>
2026-01-19 16:04:16 -0300

    Adicionando arquivos

--- TasFlowApp/src/main/java/com/taskflow/TasFlowApp/usecase/CreateUserUseCase.java
+++ TasFlowApp/src/main/java/com/taskflow/TasFlowApp/usecase/CreateUserUseCase.java
@@ -1,24 +1,26 @@
 package com.taskflow.TasFlowApp.usecase;
 
-
 import com.taskflow.TasFlowApp.application.dto.UserDto;
 import com.taskflow.TasFlowApp.domain.entity.User;
 import com.taskflow.TasFlowApp.infra.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class CreateUserUseCase {
     private final UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
 
     public void execute(UserDto userDto) {
         User user = User.builder()
                 .name(userDto.name)
                 .email(userDto.email)
-                .password(userDto.password)
+                .password(passwordEncoder.encode(userDto.password))
                 .phone(userDto.phone)
                 .build();
+        
         this.userRepository.save(user);
     }
 }
